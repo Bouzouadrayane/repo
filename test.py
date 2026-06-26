622f92c2bea25d917c920620c44533d6eb01f187
Bouzouad Rayane <bouzouadrayane81@gmail.com>
2026-06-26 10:35:03 +0100

    aller on y va

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-print("hello pipeline how are you test")
-print("je suis la deuxieme branche")
-
-<<<<<<< HEAD
-print('mail')
-print("mister sambousa")
-=======
-
-print("mister sambousa")
-
-print("yakooul yal kbch")
->>>>>>> d00a3ae1ea1ff25e4ce7ff25d36090cada9bc807
+print('hello world')
